bd77e4f525b8a35db76d544fcf41ba913990327d
moazzamali922 <121150980+moazzamali922@users.noreply.github.com>
2023-05-18 00:06:50 +0500

    Weather App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+// import Resturant from './Component/Basic/Resturant';
+import Temp from './Weather Component/temp';
+
+export const App = () => {
+  return (
+    // <div><Resturant/></div>
+      <div><Temp/></div>
+    
+     
+  )
+}
+
+export default App;
